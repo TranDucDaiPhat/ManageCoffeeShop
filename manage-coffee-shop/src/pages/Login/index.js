8cbfd37a983a8822ed2315611fd4114cc73a25ca
luan2410 <loctruongluan12a5hd@gmail.com>
2025-04-22 23:01:33 +0700

    CRUD prodcuct và ratelimite clien, merge nhánh Fontend

--- manage-coffee-shop/src/pages/Login/index.js
+++ manage-coffee-shop/src/pages/Login/index.js
@@ -1,30 +1,73 @@
-import { useState } from "react";
+import { useRef,useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { useAuth } from "../../AuthContext";
 import styles from './Login.module.css'
-import { FaSpinner } from "react-icons/fa";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function Login() {
     const [username, setUsername] = useState("admin");
     const [password, setPassword] = useState("admin");
+    const [showPassword, setShowPassword] = useState(false); // ✅ Thêm state để hiển thị mật khẩu
 
     const [isLoading, setIsLoading] = useState(false);
 
     const navigate = useNavigate();
     const { role, setAccessToken } = useAuth();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+    // const handleLogin = async (e) => {
+    //     e.preventDefault();
 
     
+    //     if (username.trim() === '' || password.trim() === '') {
+    //         toast.error("Vui lòng nhập username và password!");
+    //         return;
+    //     }
+    
+    //     setIsLoading(true); // Bắt đầu loading
+    
+    //     try {
+    //         const res = await fetch("http://localhost:8081/myapp/api/business/auth/login", {
+    //             method: "POST",
+    //             headers: { "Content-Type": "application/json" },
+    //             body: JSON.stringify({ username: username.trim(), password: password }),
+    //             credentials: "include",
+    //         });
+    
+    //         if (!res.ok) {
+    //             // Nếu response có status mà không phải kết nối lỗi mạng
+    //             if (res.status === 401 || res.status === 403) {
+    //                 toast.error("Đăng nhập thất bại! Kiểm tra tài khoản và mật khẩu.");
+    //             } else {
+    //                 toast.error(`Lỗi từ server: ${res.status}`);
+    //             }
+    //             return;
+    //         }
+    
+    //         const data = await res.json();
+    //         sessionStorage.setItem("accessToken", data.token);
+    //         setAccessToken(data.token);
+    
+    //         navigate("/tao-hoa-don");
+    //     } catch (error) {
+    //         console.error("Lỗi khi đăng nhập:", error.message);
+    
+    //         // Nếu không có phản hồi từ server (ví dụ server tắt)
+    //         toast.error("Không thể kết nối đến server!");
+    //     } finally {
+    //         setIsLoading(false); // Tắt loading
+
+    //     }
+    // };
+    
+    const handleLogin = async (e) => {
+        e.preventDefault();
         if (username.trim() === '' || password.trim() === '') {
             toast.error("Vui lòng nhập username và password!");
             return;
         }
-    
-        setIsLoading(true); // Bắt đầu loading
     
         try {
             const res = await fetch("http://localhost:8081/myapp/api/business/auth/login", {
@@ -34,33 +77,31 @@ function Login() {
                 credentials: "include",
             });
     
-            if (!res.ok) {
-                // Nếu response có status mà không phải kết nối lỗi mạng
-                if (res.status === 401 || res.status === 403) {
-                    toast.error("Đăng nhập thất bại! Kiểm tra tài khoản và mật khẩu.");
-                } else {
-                    toast.error(`Lỗi từ server: ${res.status}`);
-                }
+            if (res.status === 429) {
+                const errorData = await res.json();
+                toast.error(errorData.token || "Bạn đã vượt quá số lần đăng nhập. Vui lòng thử lại sau.");
                 return;
             }
     
+            if (!res.ok) {
+                throw new Error(`Lỗi đăng nhập: ${res.status}`);
+            }
+    
             const data = await res.json();
+            console.log("token: ", data);
             sessionStorage.setItem("accessToken", data.token);
             setAccessToken(data.token);
     
             navigate("/tao-hoa-don");
         } catch (error) {
             console.error("Lỗi khi đăng nhập:", error.message);
-    
-            // Nếu không có phản hồi từ server (ví dụ server tắt)
-            toast.error("Không thể kết nối đến server!");
-        } finally {
-            setIsLoading(false); // Tắt loading
-
+            toast.error("Đăng nhập thất bại! Kiểm tra tài khoản và mật khẩu.");
         }
     };
     
-    
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+      };
 
     return (
         <div className={styles.container}>
@@ -79,23 +120,29 @@ function Login() {
                             onChange={(e) => setUsername(e.target.value)}
                             required
                         />
-                        <input
-                            type="password"
-                            placeholder="Mật khẩu"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <button type="submit" disabled={isLoading}>
-                            {isLoading ? (
-                                <span style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-                                    <FaSpinner className={styles.spinner} />
-                                    Đang đăng nhập...
-                                </span>
-                            ) : (
-                                "Đăng Nhập"
-                            )}
-                        </button>
+                        
+                       <div style={{ position: 'relative' }}>
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Nhập mật khẩu"
+                                style={{ paddingRight: '30px' }} // Đảm bảo có khoảng trống cho icon
+                            />
+                            <div
+                                style={{
+                                position: 'absolute',
+                                right: '8px',
+                                top: '50%',
+                                transform: 'translateY(-50%)',
+                                cursor: 'pointer',
+                                }}
+                                onClick={togglePasswordVisibility}
+                            >
+                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                            </div>
+                        </div>
+                        <button type="submit">Đăng Nhập</button>
                         <a href="#" className={styles.forgot_password}>Quên mật khẩu?</a>
                     </form>
                 </div>
