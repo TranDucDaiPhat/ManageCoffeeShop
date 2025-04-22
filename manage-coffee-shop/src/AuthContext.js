c39d605d65b945d54085f05f0ef61d33d9fc3d32
thuyduong0809 <thuyduongheocon809@gmail.com>
2025-04-22 22:19:58 +0700

    update code employee

--- manage-coffee-shop/src/AuthContext.js
+++ manage-coffee-shop/src/AuthContext.js
@@ -1,10 +1,9 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {jwtDecode} from "jwt-decode"
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
   const [role, setRole] = useState(() => {
     const token = sessionStorage.getItem("accessToken");
     if (token) {
@@ -19,9 +18,10 @@ export const AuthProvider = ({ children }) => {
     return null;
   });
 
-
-  const [accessToken, setAccessToken] = useState(() => sessionStorage.getItem("accessToken") || null);
-  const [employeeId, setEmployeeId] = useState(null)
+  const [accessToken, setAccessToken] = useState(
+    () => sessionStorage.getItem("accessToken") || null
+  );
+  const [employeeId, setEmployeeId] = useState(null);
 
   // Hàm đăng xuất
   const logout = () => {
@@ -32,29 +32,33 @@ export const AuthProvider = ({ children }) => {
     window.location.href = "/";
   };
 
-
   const setUserInfo = async () => {
     if (!accessToken) return;
-  
+
     try {
       const decoded = jwtDecode(accessToken);
-      console.log(decoded)
+      console.log(decoded);
       setRole(decoded.scope || null);
-      setEmployeeId(decoded.employeeId)
+      setEmployeeId(decoded.employeeId);
     } catch (error) {
       console.warn("Không thể giải mã token:", error);
       logout();
     }
   };
-  
 
   useEffect(() => {
     if (accessToken) {
-      setUserInfo(); 
+      setUserInfo();
     }
   }, [accessToken]);
 
-  return <AuthContext.Provider value={{ role, accessToken, setAccessToken, logout, employeeId }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider
+      value={{ role, accessToken, setAccessToken, logout, employeeId }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
